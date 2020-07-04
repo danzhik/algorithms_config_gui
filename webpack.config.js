1aa1e9e4b4d23109393a70ddc7a88e1bfcf2446b
daniilzhitnitskii <daniil@movendos.com>
2020-07-04 14:53:37 +0300

    Initial commit. Set-up scaffolded react app with webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const path = require("path");
+
+module.exports = {
+    entry: {index: path.resolve(__dirname, "src", "index.jsx")},
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "build")
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            }
+        ]
+    },
+    optimization: {
+        splitChunks: { chunks: "all" }
+    },
+    plugins: [
+    new HtmlWebPackPlugin({
+        template: path.resolve(__dirname, "public", "index.html"),
+        filename: './index.html',
+        favicon: './public/favicon.ico'
+    })
+]
+}
